d984fe1298e9246c26bad595b84a5d63220ef4a9
CrisBMoya <tobal.92@gmail.com>
2019-09-17 11:36:29 -0300

    Update DownSamplingInR.R

--- DownSamplingInR.R
+++ DownSamplingInR.R
@@ -3,8 +3,8 @@ library(tidyverse)
 library(dummies)
 library(UBL)
 
-TrainTransaction=read_delim(file='C:/Users/Tobal/Google Drive/Github/IEEE_Fraud_Detection/Data/train_transaction.csv.zip', delim=',')
-TestTransaction=read_delim(file='C:/Users/Tobal/Google Drive/Github/IEEE_Fraud_Detection/Data/test_transaction.csv.zip', delim=',')
+TrainTransaction=read_delim(file='~/Data/train_transaction.csv.zip', delim=',')
+TestTransaction=read_delim(file='~/Data/test_transaction.csv.zip', delim=',')
 
 TrainTransaction$QuantileAmt=cut(x=TrainTransaction$TransactionAmt, breaks=20, labels=paste0("C",1:20))
 TestTransaction$QuantileAmt=cut(x=TestTransaction$TransactionAmt, breaks=20, labels=paste0("C",1:20))
